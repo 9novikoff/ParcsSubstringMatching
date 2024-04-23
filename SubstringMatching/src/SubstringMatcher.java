0da25b8e07d93b75a3a89ee170efe04e4c412240
9novikoff <novikov.oleksandr10@gmail.com>
2024-04-24 00:49:55 +0300

    little fixes7

--- SubstringMatching/src/SubstringMatcher.java
+++ SubstringMatching/src/SubstringMatcher.java
@@ -8,7 +8,7 @@ public class SubstringMatcher implements AM {
         String text = (String)info.parent.readObject();
         String pattern = (String)info.parent.readObject();
 
-        List<Integer> res = new ArrayList<Integer>();
+        ArrayList<Integer> res = new ArrayList<Integer>();
 
         for (int i = 0; i < text.length() - pattern.length() + 1; i++) {
             boolean isMatch = true;
